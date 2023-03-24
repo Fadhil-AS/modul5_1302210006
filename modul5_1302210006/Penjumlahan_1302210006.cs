5d16b93143a9a0f6d2e69d151d12139d4dafc34f
Muhammad Fadhil Ardiansyah Supiyan <mfadhil661@gmail.com>
2023-03-24 08:01:49 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_1302210006/Penjumlahan_1302210006.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302210006
+{
+    internal class Penjumlahan_1302210006<T>
+    {
+        private T jumlah;
+        public void JumlahTigaAngka_1302210006(T x, T y, T z) {
+            this.jumlah = jumlah;
+            jumlah = (dynamic) x + (dynamic) y + (dynamic) z;
+            Console.WriteLine("Jumlah dari tiga angka " + jumlah);
+            
+        }
+    }
+}
--- modul5_1302210006/Program.cs
+++ modul5_1302210006/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul5_1302210006;
+
+int x = 13;
+int y = 02;
+int z = 21;
+Penjumlahan_1302210006<int> jum = new Penjumlahan_1302210006<int>();
+jum.JumlahTigaAngka_1302210006(x, y, z);
+
+
